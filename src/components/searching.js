ebbedb87c030a7c9cde8c0fb22b6336a3ed32d85
dUbislav <sashadeev80@gmail.com>
2026-02-09 15:30:02 +0300

    PR6.2

--- src/components/searching.js
+++ src/components/searching.js
@@ -3,9 +3,15 @@ import {rules, createComparison} from "../lib/compare.js";
 
 export function initSearching(searchField) {
     // @todo: #5.1 — настроить компаратор
+    const searchRule = rules.searchMultipleFields(
+        searchField,                    // имя поля поиска в состоянии
+        ['date', 'customer', 'seller'], // поля для поиска
+        false                           // без учета регистра
+    );
+    const compare = createComparison(['skipEmptyTargetValues'], [searchRule]);
 
     return (data, state, action) => {
         // @todo: #5.2 — применить компаратор
-        return data;
+        return data.filter(row => compare(row, state));
     }
 }
